ccd202ceb62f389bb684660bc6ef64761265e823
burakSencan <burak.sencan.1995@hotmail.com>
2022-02-01 00:13:59 +0300

    Note3 added

---
+++ section-1-2-3-4/script.js
@@ -0,0 +1,32 @@
+"use strict";
+/* for Note 3 Section animated div section */
+const parent = document.getElementById("note3ParentBox");
+const child = document.getElementById("note3ChildBox");
+const Note3SpanTop = document.getElementById("Note3SpanTop");
+const Note3SpanLeft = document.getElementById("Note3SpanLeft");
+
+const interval = setInterval(changePosition, 1000);
+
+let topProperty;
+let leftProperty;
+
+function changePosition() {
+  topProperty = getRandomNum();
+  leftProperty = getRandomNum();
+
+  
+  child.style.top = topProperty + "px";
+  child.style.left = leftProperty + "px";
+
+  Note3SpanTop.innerText = child.style.top
+  Note3SpanLeft.innerText = child.style.left
+}
+changePosition();
+
+function getRandomNum() {
+  return Math.floor(Math.random() * 200 + 100);
+}
+
+function setPosInfo(topProperty,leftProperty) {
+  
+}
