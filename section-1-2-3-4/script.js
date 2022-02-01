33d98bac5d78d098dffe58ad9d4278577d3aabeb
burakSencan <burak.sencan.1995@hotmail.com>
2022-02-02 01:27:37 +0300

    Note4Added

--- section-1-2-3-4/script.js
+++ section-1-2-3-4/script.js
@@ -24,3 +24,45 @@ function changePosition() {
 function getRandomNum() {
   return Math.floor(Math.random() * 200 + 100);
 }
+
+/* for Note 4 Section animated div section */
+const note4leftdiv = document.getElementById("note4LeftDiv");
+const note4rightdiv = document.getElementById("note4RightDiv");
+const note4leftP = document.getElementById("note4LeftPDiv");
+const note4rightP = document.getElementById("note4RightPDiv");
+const note4RightContent = document.getElementById("note4RightContent");
+console.log(note4RightContent);
+let note4leftdivW = 250;
+let note4RightContentW = 150;
+let increase = true;
+
+const interval2 = setInterval(changeWidth, 80);
+function changeWidth() {
+  if (note4leftdivW === 300) {
+    increase = false;
+  } else if (note4leftdivW === 250) {
+    increase = true;
+  }
+
+  if (increase) {
+    note4leftdivW++;
+    note4RightContentW++;
+    note4leftdiv.style.width = note4leftdivW + "px";
+    note4leftdiv.style.height = note4leftdivW + "px";
+    note4leftP.innerText = `Total Width: ${note4leftdivW}px`;
+
+    note4RightContent.style.width = note4RightContentW + "px";
+    note4RightContent.style.height = note4RightContentW + "px";
+    note4rightP.innerText = `Content Width: ${note4leftdivW}px`;
+  } else {
+    note4leftdivW--;
+    note4RightContentW--;
+    note4leftdiv.style.width = note4leftdivW + "px";
+    note4leftdiv.style.height = note4leftdivW + "px";
+    note4leftP.innerText = `Total Width: ${note4leftdivW}px`;
+
+    note4RightContent.style.width = note4RightContentW + "px";
+    note4RightContent.style.height = note4RightContentW + "px";
+    note4rightP.innerText = `Content Width: ${note4leftdivW}px`;
+  }
+}
