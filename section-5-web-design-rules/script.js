0e197330b81b1016d44550e2d47e90e8abf00f57
burakSencan <burak.sencan.1995@hotmail.com>
2022-02-15 23:45:40 +0300

    adding-web-desing-rules

---
+++ section-5-web-design-rules/script.js
@@ -0,0 +1,41 @@
+const note_1_p = document.querySelectorAll(".note-1-p");
+const note_1_img_1 = document.querySelector("#note-1-img-1");
+const note_1_img_2 = document.querySelector("#note-1-img-2");
+
+note_1_p.forEach((p, idx) => {
+  p.addEventListener("click", () => {
+    console.log(idx);
+    if (idx === 0) {
+      note_1_img_1.src = "./images/luxury.png";
+      note_1_img_2.src = "./images/luxury-2.png";
+    }
+    if (idx === 1) {
+      note_1_img_1.src = "./images/simple-1.png";
+      note_1_img_2.src = "./images/simple-2.png";
+    }
+    if (idx === 2) {
+      note_1_img_1.src = "";
+      note_1_img_2.src = "";
+    }
+    if (idx === 3) {
+      note_1_img_1.src = "";
+      note_1_img_2.src = "";
+    }
+    if (idx === 4) {
+      note_1_img_1.src = "";
+      note_1_img_2.src = "";
+    }
+    if (idx === 5) {
+      note_1_img_1.src = "";
+      note_1_img_2.src = "";
+    }
+    if (idx === 6) {
+      note_1_img_1.src = "";
+      note_1_img_2.src = "";
+    }
+    if (idx === 7) {
+      note_1_img_1.src = "";
+      note_1_img_2.src = "";
+    }
+  });
+});
