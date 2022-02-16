dbf5a5b72626fe29fed237f35122cc66b18b2238
burakSencan <burak.sencan.1995@hotmail.com>
2022-02-16 22:59:25 +0300

    note-1-done

--- section-5-web-design-rules/script.js
+++ section-5-web-design-rules/script.js
@@ -4,6 +4,7 @@ const note_1_img_2 = document.querySelector("#note-1-img-2");
 
 note_1_p.forEach((p, idx) => {
   p.addEventListener("click", () => {
+
     console.log(idx);
     if (idx === 0) {
       note_1_img_1.src = "./images/luxury.png";
@@ -14,28 +15,24 @@ note_1_p.forEach((p, idx) => {
       note_1_img_2.src = "./images/simple-2.png";
     }
     if (idx === 2) {
-      note_1_img_1.src = "";
-      note_1_img_2.src = "";
+      note_1_img_1.src = "./images/plain-1.png";
+      note_1_img_2.src = "./images/plain-2.png";
     }
     if (idx === 3) {
-      note_1_img_1.src = "";
+      note_1_img_1.src = "./images/bold-1.png";
       note_1_img_2.src = "";
     }
     if (idx === 4) {
-      note_1_img_1.src = "";
-      note_1_img_2.src = "";
+      note_1_img_1.src = "./images/calm-1.png";
+      note_1_img_2.src = "./images/calm-2.png";
     }
     if (idx === 5) {
-      note_1_img_1.src = "";
+      note_1_img_1.src = "./images/startup-1.png";
       note_1_img_2.src = "";
     }
     if (idx === 6) {
-      note_1_img_1.src = "";
-      note_1_img_2.src = "";
-    }
-    if (idx === 7) {
-      note_1_img_1.src = "";
-      note_1_img_2.src = "";
+      note_1_img_1.src = "./images/fun-1.png";
+      note_1_img_2.src = "./images/fun-2.png";
     }
   });
 });
